9f17620a67db547567b5712a055dec6ae3730768
Ajeesh <ajeeshavi@gmail.com>
2019-06-30 00:55:40 +0900

    [artf3002]Addition complette

--- first.py
+++ first.py
@@ -1,4 +1,9 @@
 ######Calculator program
+def add(x, y):
+
+    return x + y
+
+######Start
 print("CALCULATOR PROGRAM")
 print("select operation")
 print("1.ADD")
@@ -7,3 +12,11 @@ print("3.MUL")
 print("4.DIV")
 print("5.SQR")
 choice=input("Enter choice (1/2/3/4/5):")
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+
+if choice =='1':
+  print(num1, "+", num2, "=", add(num1,num2))
+    
+    #else
+    #print("invalid")
