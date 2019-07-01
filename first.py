a759536c9747c55bcde5c250031b6f93e1eb5fe6
Ajeesh <ajeeshavi@gmail.com>
2019-07-02 06:55:46 +0900

    [artf3002] first nueral network with random weigts

--- first.py
+++ first.py
@@ -1,8 +1,19 @@
 ######First AI program
+print("NUERL PROGRAM")
 import numpy as np
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 ######Start
-print("NUERAL  PROGRAM")
-num1=int(input("Enter the first number:"))
-print("outofsig:", sigmoid(num1))
+training_input=np.array([[0,0,1],[1,1,1],[1,0,0],[0,1,1]] )
+training_output=np.array([0,1,1,0] ).T
+np.random.seed(1)
+synaptic_weights=2*np.random.random((3,1))-1
+print("Random weigts:")
+print(synaptic_weights)
+for iteration in range (1):
+    input_layer=training_input;
+    output=sigmoid(np.dot(input_layer,synaptic_weights))
+#num1=int(input("Enter the first number:"))
+#print("outofsig:", sigmoid(num1))
+print("output after training:")
+print(output)
