45c6553a4461f68e3f31830ed45c1fae50267f23
Ajeeshviswambharan <n2nychannel@gmail.com>
2025-06-30 20:46:34 +0530

    First commit for Pybmm base

---
+++ battery.py
@@ -0,0 +1,24 @@
+import pybamm
+import pandas as pd
+import numpy as np
+
+# Load model and parameter values
+model = pybamm.lithium_ion.SPM()
+param = model.default_parameter_values
+
+# Create and solve the simulation
+sim = pybamm.Simulation(model, parameter_values=param)
+solution=sim.solve([0, 3600])  # 1-hour simulation
+time = solution["Time [s]"].entries
+voltage = solution["Terminal voltage [V]"].entries
+
+# Save to CSV
+df = pd.DataFrame({
+    "Time (s)": time,
+    "Voltage (V)": voltage
+})
+
+df.to_csv("battery_simulation_output.csv", index=False)
+
+# Plot results
+sim.plot()
